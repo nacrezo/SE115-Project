100daac3d7dc95ed88eef745ce0ea0a3c5a74a2d
Can Özer <canozer.pirireis@gmail.com>
2022-12-09 20:12:04 +0300

    Changes for Card class...
    * 2 private integers added...
    * toPrint function added...

--- Card.java
+++ Card.java
@@ -1,1 +1,14 @@
 public class Card {
+	private String numbers;
+	private String shapes;
+	
+	public Card(String cardNumbers, String cardShapes) {
+		numbers = cardNumbers;
+		shapes = cardShapes;
+	}
+	public String toPrint(){
+		return "number:" +numbers "shape:" +shapes; 
+	}
+}
+		
+		
