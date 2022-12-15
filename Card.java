0fb95e2c1e31fd212b52b73ca9b097455149e836
Can Özer <canozer.pirireis@gmail.com>
2022-12-16 01:12:42 +0300

    İmprovements...
    String names changed, shuffle function changed, dealCard() function added.

--- Card.java
+++ Card.java
@@ -1,13 +1,19 @@
 public class Card {
-	private String numbers;
-	private String shapes;
+	private String faces;
+	private String suits;
 	
-	public Card(String cardNumbers, String cardShapes) {
-		numbers = cardNumber;
-		shapes = cardShape;
+	public Card(String a, String b) {
+		faces = a;
+		suits = b;
 	}
+	public String getA() {return faces;}
+	public void setA(String a) {faces = a;}
+	
+	public String getB() {return suits;}
+	public void setB(String b) {suits = b;}
+	
 	public String toPrint(){
-		return "number:" +numbers "shape:" +shapes; 
+		return faces + "of" + suits;
 	}
 }
 		
--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,27 +1,44 @@
 import java.util.Random;
 
 public class DeckOfCards {
-	Random r = new Random(System.currentTimeMillis());
+	Random r = new Random();
 	private Card[] deck;
+	private int current_card;
 
 	public DeckOfCards() {
-		String[] cardNumbers = { "Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
-		String[] cardShapes = { "Hearth", "Diamond", "Club", "Spade" };
+		current_card = 0;
+		
+		String[] faces = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+		
+		String[] suits = { "u0003", "u0004", "u0005", "u0006" };
+		
         deck = new Card[52];
        
         for(int i = 0; i<deck.length; i++) {
-        deck[i] = new Card(cardNumbers[i%13], cardShapes[i/13]);
+        deck[i] = new Card(faces[i%13], suits[i/13]);
 		}
 	}
 
     public void shuffle() {
+		
+		current_card = 0;
 	   
 	   for(int first_card = 0; first_card < deck.length; first_card++) {
 		   int second_card = r.nextInt(52);
+		   
 		   Card temp_card = deck[first_card];
 	       deck[first_card] = deck[second_card];
+		   deck[second_card] = temp_card;
 	    }
 	}
+	
+	public Card dealCard() {
+		if(current_card < deck.length) {
+			return deck[current_card++];
+		} else {
+			return null;
+		}
+    }
 }
   
 		   
